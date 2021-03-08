81b497bba7b6d55b61baf749d681f09efe323646
PratibhaMastud <67584081+PratibhaMastud@users.noreply.github.com>
2021-03-08 15:01:26 +0530

    Book Store Project

---
+++ src/apiConstants/UserApiConstants.js
@@ -0,0 +1,13 @@
+module.exports = {
+    loginPath :"/login",
+    registrationPath :"/registration",
+    getBookPath: "/get/book",
+    addCard: "/add_cart_item/",
+    getaddedBook: "/get_cart_items",
+    cart_quantity: "/cart_item_quantity/",
+    remove_cart: "/remove_cart_item/",
+    add_wishList: "/add_wish_list/",
+    removerWishList: "/remove_wishlist_item/",
+    get_wishList: "/get_wishlist_items",
+    addOrder: "/add/order",
+}
